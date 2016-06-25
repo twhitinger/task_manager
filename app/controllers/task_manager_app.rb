4044ecd5306c19b50d3f61c4f5896358a588244b
Trent Whitinger <twhitinger@gmail.com>
2016-06-25 14:09:34 -0600

    task manager

---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,34 @@
+require 'models/task_manager'
+
+class TaskManagerApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = task_manager.all
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    task_manager.create(params[:task])
+    redirect '/tasks'
+    # "<p>Params: #{params}</p> <p>Task params: #{params[:task]}</p>"
+  end
+
+  get '/tasks/:id' do |id|
+    @task = task_manager.find(id.to_i)
+    erb :show
+  end
+  
+  def task_manager
+    database = YAML::Store.new('db/task_manager')
+    @task_manager ||= TaskManager.new(database)
+  end
+end
